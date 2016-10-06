163e9d81bbcfb8c5f9597cd073acc3ad93dea979
Artem Lysenko <artem.lysenko@riken.jp>
2016-10-06 14:17:13 +0900

    Update Main.java

--- src/main/java/jp/riken/clustering/Main.java
+++ src/main/java/jp/riken/clustering/Main.java
@@ -55,7 +55,7 @@ public class Main {
 			System.out.println("\t(2)Number of CPU threads to use.");
 			System.out.println("\t(3)Maximum module size (hard limit --  all modules produced will be below this size)");
 			System.out.println("\t(4)Minimum module size (soft limit -- some modules produced may be below this size as maximum module size takes priority).");
-			System.out.println("\t(5)[optional] put in 'experimenatl' word as argument in the last position for density tree to be cut at the highest density edges rather than the lowest ones.");
+			System.out.println("\t(5)[optional] put in 'experimental' word as argument in the last position for density tree to be cut at the highest density edges rather than the lowest ones.");
 			System.out.println("Outputs: a .tree_file containing the generated density tree and .pascal file with moudles in PASCAL tool format (each line starts with cluster id followed by cluster confidence number (not used, so always set to 1.0) then followed by ids of cluster members).");
 			return;
 		} else {
